e1d89959b2a994d933fb7dff45990429adf43c19
Felice Carotenuto <felice.carotenuto@gmail.com>
2025-02-21 14:56:59 +0100

    Update template.js

--- template.js
+++ template.js
@@ -17,31 +17,49 @@ function sendGetRequest() {
         headers: {'Content-Type': 'application/json', }, 
         method: 'GET'
     };
+    
     if (data.authFlow === 'own') {
         params.authorization = auth;
     }
+    
     return sendHttpRequest(requestUrl, params).then(successResult => {
         let bodyParsed = JSON.parse(successResult.body);
-
+        
         if (successResult.statusCode >= 200 && successResult.statusCode < 400) {
             if (data.type === 'cell') {
                 return bodyParsed.values[0][0];
             }
-
+            
             if (data.type === 'object') {
-                return bodyParsed.values.reduce((acc, curr) => {
-                    acc[curr[0]] = curr[1];
-                    return acc;
-                }, {});
+                // Ottiene le intestazioni dalla prima riga
+                const headers = bodyParsed.values[0];
+                
+                // Crea un array di oggetti, uno per ogni riga (esclusa la prima riga delle intestazioni)
+                const result = [];
+                
+                // Itera su ogni riga a partire dalla seconda
+                for (let i = 1; i < bodyParsed.values.length; i++) {
+                    const row = bodyParsed.values[i];
+                    const obj = {};
+                    
+                    // Per ogni cella nella riga, utilizza l'intestazione corrispondente come chiave
+                    for (let j = 0; j < headers.length; j++) {
+                        // Se il valore nella posizione corrente esiste, assegnalo; altrimenti, usa una stringa vuota
+                        obj[headers[j]] = j < row.length ? row[j] : '';
+                    }
+                    
+                    result.push(obj);
+                }
+                
+                return result;
             }
-
+            
             return bodyParsed.values;
         } else {
             return '';
         }
     });
 }
-
 
 function getUrl() {
     if (data.authFlow === 'stape') {
@@ -60,7 +78,7 @@ function getUrl() {
           '&range=' + enc(data.type === 'cell' ? data.cell : data.range)
         );
     }
-
+    
     return 'https://content-sheets.googleapis.com/v4/spreadsheets/'+spreadsheetId+'/values/'+enc(data.type === 'cell' ? data.cell : data.range);
 }
 
